cd7e0803139d9fdfb7fd3511ea83f539140f9aab
Gustavo Henrique de Moraes <gustavomraes104@gmail.com>
2024-04-03 22:07:37 -0300

    Salvando arquivos

--- Assets/Scripts/EntrarFase.cs
+++ Assets/Scripts/EntrarFase.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class EntrarFase : MonoBehaviour
 {
+
+    public string fase;
     // Start is called before the first frame update
     void Start()
     {
+
+       if (true)
+        {
+
+        }
         
+
+        //if (interagiu com a pasta){
+        //SceneManager.LoadScene("fase");
+        //}
     }
 
     // Update is called once per frame
