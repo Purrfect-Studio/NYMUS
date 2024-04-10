905646281c70e44f994c8a5d4877ee171fe2cfb6
Gustavo Henrique de Moraes <gustavomraes104@gmail.com>
2024-04-09 23:39:43 -0300

    NYM-23 Estrutura do menu
    (Falta fazer o estilo pra ficar bonito)

---
+++ NYMUS/Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevel;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene(nomeDoLevel);
+    }
+    public void AbrirOpcoes()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+    public void FecharOpcoes()
+    {
+        painelOpcoes.SetActive(false);
+        painelMenuInicial.SetActive(true);
+        
+    }
+    public void Sair()
+    {
+        Debug.Log("Sair do Jogo");
+        Application.Quit();
+    }
+}
