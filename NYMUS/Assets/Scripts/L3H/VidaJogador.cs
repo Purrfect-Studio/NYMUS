dd6c19c5ae05a0acc074c56ff227c80270ba86f1
Caetano Costa <caetanogonca@gmail.com>
2024-04-16 00:49:46 -0300

    Nym-18 cookie causa dano no L3H + frames de invulnerabilidade

--- NYMUS/Assets/Scripts/L3H/VidaJogador.cs
+++ NYMUS/Assets/Scripts/L3H/VidaJogador.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class VidaJogador : MonoBehaviour
 {
+    public static bool invulneravel;
     public float vidaMaxima;
     [SerializeField] private float vidaAtual;
+    [SerializeField] private SpriteRenderer sprite;
     // Start is called before the first frame update
     void Start()
     {
         vidaAtual = vidaMaxima;
+        invulneravel = false;
     }
 
     // Update is called once per frame
@@ -21,5 +24,20 @@ public class VidaJogador : MonoBehaviour
     public void tomarDano(float danoTomado)
     {
         vidaAtual -= danoTomado;
+        invulneravel = true;
+        StartCoroutine (invulnerabilidade());
+    }
+
+    IEnumerator invulnerabilidade()
+    {
+        
+        for(float i = 0f; i < 1f; i += 0.1f)
+        {
+            sprite.enabled = false;
+            yield return new WaitForSeconds(0.1f);
+            sprite.enabled = true;
+            yield return new WaitForSeconds(0.1f);
+        }
+        invulneravel = false;
     }
 }
