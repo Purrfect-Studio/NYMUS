4191cc62650480f58bd0c7496875a1c11319027b
Caetano Costa <caetanogonca@gmail.com>
2024-04-23 17:26:08 -0300

    espinhos prontos

--- NYMUS/Assets/Scripts/Inimigos/Espinho.cs
+++ NYMUS/Assets/Scripts/Inimigos/Espinho.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-
-public class DanoNoJogador : MonoBehaviour
+public class Espinho : MonoBehaviour
 {
-    public CircleCollider2D ccCookie;
+    public BoxCollider2D bcEspinho;
     public Transform jogador;
     [SerializeField] private UnityEvent DanoCausado;
     [SerializeField] private LayerMask layerJogador; //Variavel de apoio para rechonhecer a layer do chao;
-    
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -24,9 +23,9 @@ public class DanoNoJogador : MonoBehaviour
     {
         if (colisaoJogador() == true)
         {
-            if(VidaJogador.invulneravel == false)
+            if (VidaJogador.invulneravel == false)
             {
-                if(transform.position.x <= jogador.transform.position.x)
+                if (transform.position.x <= jogador.transform.position.x)
                 {
                     VidaJogador.knockbackParaDireita = -1;
                 }
@@ -41,7 +40,7 @@ public class DanoNoJogador : MonoBehaviour
 
     private bool colisaoJogador()
     {
-        RaycastHit2D colisao = Physics2D.BoxCast(ccCookie.bounds.center, ccCookie.bounds.size, 0, Vector2.down, 0.05f, layerJogador); // Cria um segundo box collider para reconhecer o jogador
+        RaycastHit2D colisao = Physics2D.BoxCast(bcEspinho.bounds.center, bcEspinho.bounds.size, 0, Vector2.down, 0.05f, layerJogador); // Cria um segundo box collider para reconhecer o jogador
         return colisao.collider != null; //Retorna um valor verdadeiro, dizendo que encostou no jogador
     }
 }
