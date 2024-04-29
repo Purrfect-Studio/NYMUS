cb61c032f4d5a68218d41a193628ad4447ddf196
Gustavo Henrique de Moraes <gustavomraes104@gmail.com>
2024-04-29 15:33:18 -0300

    Nao da mais para grudar 2 caixas ao mesmo tempo

--- NYMUS/Assets/Scripts/Objetos/GrudarObjeto.cs
+++ NYMUS/Assets/Scripts/Objetos/GrudarObjeto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,16 @@ public class GrudarObjeto : MonoBehaviour
     public Transform posicaoPlayer;
     public float distanciaMinima = 1f; // Distância mínima entre o jogador e a caixa
 
-    private bool estaGrudado = false; // Verifica se a caixa está grudada no jogador
+    static public bool estaEmpurrando; // Verifica se o jogador está grudado em alguma caixa
+    public bool estaGrudado; //Verifica se a caixa específica está sendo empurrada
 
     private Vector3 diferencaPosicao; // Armazena a diferença de posição inicial entre o jogador e a caixa
 
     void Start()
     {
         // Calcula a diferença de posição inicial entre o jogador e a caixa
+        estaEmpurrando = false;
+        estaGrudado = false;
         diferencaPosicao = transform.position - posicaoPlayer.position;
     }
 
@@ -28,22 +32,32 @@ public class GrudarObjeto : MonoBehaviour
 
     public void Grudar()
     {
-        if (estaGrudado==true)
-        {
-            Desgrudar();
-            return; 
-        }
-        // Define que a caixa está grudada no jogador
-        estaGrudado = true;
+        Debug.Log("Rodando");
 
-        // Calcula a diferença de posição inicial entre a caixa e o jogador
-        diferencaPosicao = transform.position - posicaoPlayer.position;
+        if (estaGrudado == true) // jogador esta empurrando ESTA caixa == true
+        {
+            Debug.Log("Desgrudou");
+            Desgrudar(); //desgruda ESTA caixa
+            return;
+        }
+        else
+        {
+            if (estaEmpurrando == false)
+            {
+                Debug.Log("Grudou");
+                estaGrudado = true;  // Define que esta caixa está grudada no jogador
+                estaEmpurrando = true; //
+                diferencaPosicao = transform.position - posicaoPlayer.position;
+            }
+        }
+       
     }
 
     public void Desgrudar()
     {
         // Define que a caixa não está mais grudada no jogador
         estaGrudado = false;
+        estaEmpurrando = false; //jogador parou de empurrar
     }
 
     private void AtualizarPosicao()
