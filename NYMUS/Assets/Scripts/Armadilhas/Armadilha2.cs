996eeb624019708ae6823a9a15fcdf0f7f0a4d9c
Caetano Costa <caetanogonca@gmail.com>
2024-05-13 21:08:57 -0300

    Fiz uma armadilha melhor

---
+++ NYMUS/Assets/Scripts/Armadilhas/Armadilha2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armadilha2 : MonoBehaviour
+{
+    public Vector2 intervaloParaAtivarArmadilha;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.enabled = false;
+    }
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(Random.Range(intervaloParaAtivarArmadilha.x, intervaloParaAtivarArmadilha.y));
+        animator.enabled = true;
+    }
+}
