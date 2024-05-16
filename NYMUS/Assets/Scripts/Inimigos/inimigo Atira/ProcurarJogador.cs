eb707249a34818a8b71dcc020645937241a3ef34
Caetano Costa <caetanogonca@gmail.com>
2024-05-16 19:18:01 -0300

    Update ProcurarJogador.cs

--- NYMUS/Assets/Scripts/Inimigos/inimigo Atira/ProcurarJogador.cs
+++ NYMUS/Assets/Scripts/Inimigos/inimigo Atira/ProcurarJogador.cs
@@ -5,24 +5,23 @@ using UnityEngine.Events;
 
 public class ProcurarJogador : MonoBehaviour
 {
-    // Variáveis para configurar a busca do jogador
     [Header("Jogador")]
     public Transform jogador; // Referência para o transform do jogador
     [Header("Layer do Jogador")]
     public LayerMask layerJogador; // Layer do jogador
-    public UnityEvent JogadorEncontrado; // Evento a ser acionado quando o jogador é encontrado
-
     [Header("Raio do circulo para detectar o jogador")]
     public float raioDetecao = 25f; // Raio de detecção do jogador
 
-    void Update()
+    public bool procurarJogador()
     {
-        // Procura o jogador dentro do raio de detecção
         Collider2D encontrarJogador = Physics2D.OverlapCircle(transform.position, raioDetecao, layerJogador);
         if (encontrarJogador != null)
-        {   
-            // Se o jogador é encontrado, aciona o evento
-            //JogadorEncontrado.Invoke();
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 
