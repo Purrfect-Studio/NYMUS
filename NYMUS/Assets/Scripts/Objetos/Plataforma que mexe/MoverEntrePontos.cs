1149f9192445b56a5dda32ba01fd39978c32287c
Caetano Costa <caetanogonca@gmail.com>
2024-05-16 22:41:16 -0300

    ajustes no script da plataforma que move

--- NYMUS/Assets/Scripts/Objetos/Plataforma que mexe/MoverEntrePontos.cs
+++ NYMUS/Assets/Scripts/Objetos/Plataforma que mexe/MoverEntrePontos.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoverEntreDoisPontos : MonoBehaviour
+public class MoverEntrePontos : MonoBehaviour
 {
     public Transform[] pontos;
     public float velocidade;
