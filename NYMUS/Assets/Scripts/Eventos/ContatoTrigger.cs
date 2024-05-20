567fb7657c5490645bf64ef11f15f0c39fc6b1f8
Beatriz Verçosa <101990596+beatrizgvercosa@users.noreply.github.com>
2024-05-19 21:43:23 -0300

    Script de glitchs feito

--- NYMUS/Assets/Scripts/Eventos/ContatoTrigger.cs
+++ NYMUS/Assets/Scripts/Eventos/ContatoTrigger.cs
@@ -5,9 +5,35 @@ using UnityEngine.Events;
 
 public class ContatoTrigger : MonoBehaviour
 {
+
+    [Header("Evento")]
     public UnityEvent evento;
+
+    [Header("Configurações")]
+    public int quantidadeAtivacoes = 0; // Quantidade de ativações permitidas
+    public bool ativarSempre = true; // Se verdadeiro, ativa o evento sempre
+
+
+    private int ativacoesRestantes; // Contador de ativações restantes
+
+    private void Start()
+    {
+        // Inicializa o contador de ativações restantes
+        ativacoesRestantes = quantidadeAtivacoes;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        evento.Invoke();
+        if (ativarSempre)
+        {
+            // Invoca o evento sempre que o gatilho for acionado
+            evento.Invoke();
+        }
+        else if (ativacoesRestantes > 0)
+        {
+            // Invoca o evento se ainda houver ativações restantes
+            evento.Invoke();
+            ativacoesRestantes--; // Decrementa o contador de ativações restantes
+        }
     }
 }
---
+++ NYMUS/Assets/Scripts/Eventos/Glitchs.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glitchs : MonoBehaviour
+{
+    
+    // Referência ao SpriteRenderer do objeto
+    private SpriteRenderer spriteRenderer;
+
+    // Referência ao Transform do objeto
+    private Transform objetoTransform;
+
+    void Start()
+    {
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        if (objetoTransform == null)
+        {
+            objetoTransform = GetComponent<Transform>();
+        }
+    }
+
+
+    // Método para flipar o eixo X
+    public void FlipX()
+    {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+    }
+
+    // Método para flipar o eixo Y
+    public void FlipY()
+    {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.flipY = !spriteRenderer.flipY;
+        }
+    }
+
+    // Método para mudar a cor do sprite para uma aleatória
+    public void MudarCorAleatoria()
+    {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = new Color(Random.value, Random.value, Random.value);
+        }
+    }
+
+    // Método para mudar a posição do objeto para um lugar próximo
+    public void MudarPosicaoAleatoria()
+    {
+        if (objetoTransform != null)
+        {
+            Vector3 novaPosicao = objetoTransform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0); 
+            objetoTransform.position = novaPosicao;
+        }
+    }
+}
