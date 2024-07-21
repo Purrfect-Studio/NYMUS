d536ba5733f30538a66a8968ec03c1981ca14212
Gustavo Henrique de Moraes <gustavomraes104@gmail.com>
2024-07-21 20:56:11 -0300

    Fazendo coração de cura

---
+++ NYMUS/Assets/Scripts/Objetos/CuraCoracao.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuraCoracao : MonoBehaviour
+{
+    [Header("Cura")]
+    public int cura; // Quantidade de vida que o coração cura
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Verifica se o objeto que colidiu tem a tag "Player"
+        if (collision.CompareTag("Jogador"))
+        {
+            // Tenta encontrar o componente VidaJogador no objeto colidido
+            VidaJogador vidaJogador = collision.GetComponent<VidaJogador>();
+            if (vidaJogador != null)
+            {
+                // Chama o método de cura no script VidaJogador
+                vidaJogador.curar(cura);
+                // Destrói o coração após a cura
+                Destroy(gameObject);
+            }
+        }
+    }
+}
