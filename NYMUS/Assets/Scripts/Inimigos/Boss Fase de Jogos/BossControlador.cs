c1f63f432f5b732040af1a84caf2fc83d69fc7c6
Caetano Costa <caetanogonca@gmail.com>
2024-07-21 21:51:44 -0300

    tiro simples do boss funcionando

--- NYMUS/Assets/Scripts/Inimigos/Boss Fase de Jogos/BossControlador.cs
+++ NYMUS/Assets/Scripts/Inimigos/Boss Fase de Jogos/BossControlador.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 
 public class BossControlador : MonoBehaviour
@@ -18,12 +19,18 @@ public class BossControlador : MonoBehaviour
         //Adicionar todos os ataques e ações do boss
     }
 
-
-
     [Header("Variáveis privadas de apoio")]
     private float cooldownRestante;
     private List<ataquesBoss> ataquesDisponiveis = new List<ataquesBoss>();
 
+    [Header("Projetil do Prefab 'Tiro Simples'")]
+    public GameObject projetilTiroSimples;
+    [Header("Atributos 'Tiro Simples'")]
+    public float velocidadeTiroSimples;
+    [SerializeField] public static float velocidadeTiroSimplesX; // Força do tiro
+    public float duracaoDoTiroSimples; // Tempo que o tiro fica no ar até ser destruído 
+    [Header("GameObject da Arma 'Tiro Simples'")]
+    public Transform armaTiroSimples; // Posição de onde o projétil será disparado
 
     // Start is called before the first frame update
     void Start()
@@ -62,6 +69,13 @@ public class BossControlador : MonoBehaviour
         {
             case ataquesBoss.TiroSimples:
                 Debug.Log("Boss está executando TiroSimples");
+                velocidadeTiroSimplesX = velocidadeTiroSimples;
+                // Instancia o projétil e define sua posição e velocidade
+                GameObject temp = Instantiate(projetilTiroSimples);
+                temp.transform.position = armaTiroSimples.position;
+                temp.GetComponent<Rigidbody2D>().velocity = new Vector2(velocidadeTiroSimplesX, 0);
+                // Destroi o projétil depois de um tempo para evitar vazamento de memória
+                Destroy(temp.gameObject, duracaoDoTiroSimples);
                 break;
 
             case ataquesBoss.TiroAmplo:
