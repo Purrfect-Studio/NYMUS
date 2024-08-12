3d8b0e04e3a846d7245382033c52902a57ab17e7
Caetano Costa <caetanogonca@gmail.com>
2024-08-12 03:07:40 -0300

    plataforma que cai

--- NYMUS/Assets/Scripts/Inimigos/inimigo Atira/InimigoParabola.cs
+++ NYMUS/Assets/Scripts/Inimigos/inimigo Atira/InimigoParabola.cs
@@ -6,23 +6,50 @@ public class InimigoParabola : MonoBehaviour
 {
     private InimigoPatrulha inimigoPatrulha;
     private AtirarEmVariosPontos atirarEmVariosPontos;
+    private ProcurarJogador procurarJogador;
+    private InimigoAtiraControlador InimigoAtiraControlador;
     // Start is called before the first frame update
     void Start()
     {
         inimigoPatrulha = GetComponent<InimigoPatrulha>();
         atirarEmVariosPontos = GetComponent<AtirarEmVariosPontos>();
+        procurarJogador = GetComponent<ProcurarJogador>();
+        InimigoAtiraControlador = GetComponent<InimigoAtiraControlador>();
+
+        InimigoAtiraControlador.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(inimigoPatrulha.direcao == 1 && atirarEmVariosPontos.velocidadeTirox < 0)
+        alterarDirecaoDoTiro();
+        atirarNaDirecaoDoJogador();
+    }
+
+    void alterarDirecaoDoTiro()
+    {
+        if (inimigoPatrulha.direcao == 1 && atirarEmVariosPontos.velocidadeTirox < 0)
         {
             atirarEmVariosPontos.velocidadeTirox *= -1;
         }
         if (inimigoPatrulha.direcao == -1 && atirarEmVariosPontos.velocidadeTirox > 0)
         {
             atirarEmVariosPontos.velocidadeTirox *= -1;
+        }
+    }
+
+    void atirarNaDirecaoDoJogador()
+    {
+        if (procurarJogador.procurarJogador())
+        {
+            if(transform.position.x - procurarJogador.jogador.transform.position.x < 0 && inimigoPatrulha.direcao == 1 || transform.position.x - procurarJogador.jogador.transform.position.x > 0 && inimigoPatrulha.direcao == -1)
+            {
+                InimigoAtiraControlador.enabled = true;
+            }
+            else
+            {
+                InimigoAtiraControlador.enabled = false;
+            }
         }
     }
 }
---
+++ NYMUS/Assets/Scripts/Objetos/PlataformaQueCai.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlataformaQueCai : MonoBehaviour
+{
+    public float tempoParaCair;
+    public float tempoParaVoltar;
+    public Rigidbody2D rigidbody2d;
+    public Vector2 posicaoInicial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        posicaoInicial = transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Jogador"))
+        {
+            StartCoroutine("PlataformaCai");
+        }
+    }
+
+    IEnumerator PlataformaCai()
+    {
+        yield return new WaitForSeconds(tempoParaCair);
+        rigidbody2d.bodyType = RigidbodyType2D.Dynamic;
+        yield return new WaitForSeconds(tempoParaVoltar);
+        rigidbody2d.bodyType = RigidbodyType2D.Kinematic;
+        transform.position = posicaoInicial;
+        rigidbody2d.velocity = Vector2.zero;
+    }
+}
