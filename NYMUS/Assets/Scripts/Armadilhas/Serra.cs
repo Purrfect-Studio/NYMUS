3f0d0f9447897e50e62deeca5c130d09f9537183
Caetano Costa <caetanogonca@gmail.com>
2024-08-13 02:33:20 -0300

    armadilha de serra

---
+++ NYMUS/Assets/Scripts/Armadilhas/Serra.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Serra : MonoBehaviour
+{
+    private float eixoZ;
+    public GameObject[] pontosDeMovimento;
+    private int indexPontos = 0;
+    private Vector2 novaPosicao;
+    public float velocidade;
+
+    public bool pararPorUmTempo = false;
+    public float tempoParado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        novaPosicao = pontosDeMovimento[indexPontos].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        giroDaSerra();
+        moverSerra();
+    }
+
+    private void giroDaSerra()
+    {
+        eixoZ += Time.deltaTime * 1000;
+        transform.rotation = Quaternion.Euler(0, 0, eixoZ);
+    }
+
+    private void moverSerra()
+    {
+        if(transform.position == pontosDeMovimento[indexPontos].transform.position)
+        {
+            indexPontos++;
+            if(indexPontos >= pontosDeMovimento.Length)
+            {
+                indexPontos = 0;
+            }
+            if (!pararPorUmTempo)
+            {
+                novaPosicao = pontosDeMovimento[indexPontos].transform.position;
+            }
+            else
+            {
+                StartCoroutine("PararPorUmTempo");
+            }
+            
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, novaPosicao, velocidade * Time.deltaTime);
+    }
+
+    IEnumerator PararPorUmTempo()
+    {
+        yield return new WaitForSeconds(tempoParado);
+        novaPosicao = pontosDeMovimento[indexPontos].transform.position;
+    }
+}
