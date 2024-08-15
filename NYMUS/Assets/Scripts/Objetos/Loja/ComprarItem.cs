37f94eb916f2606ed5e580dfc3c8659e8cedb088
Caetano Costa <caetanogonca@gmail.com>
2024-08-14 21:34:16 -0300

    loja de  consumiveis

--- NYMUS/Assets/Scripts/Objetos/Loja/ComprarItem.cs
+++ NYMUS/Assets/Scripts/Objetos/Loja/ComprarItem.cs
@@ -6,18 +6,6 @@ public class ComprarItem : MonoBehaviour
 {
     public int precoItem;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void comprar()
     {
         if(Inventario.moedasTotal - precoItem >= 0)
