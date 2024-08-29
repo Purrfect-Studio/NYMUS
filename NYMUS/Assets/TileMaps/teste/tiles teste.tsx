db8a489fc61b9832e4e3d4aed3d8ff563b024885
Caetano Costa <caetanogonca@gmail.com>
2024-08-28 23:36:05 -0300

    mapa de teste do Tiled

---
+++ NYMUS/Assets/TileMaps/teste/tiles teste.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tiles teste" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="TileSet teste automap.png" width="128" height="128"/>
+</tileset>
