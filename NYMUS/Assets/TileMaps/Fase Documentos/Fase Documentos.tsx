027a711b0a4a5dde38ac0af0b66325453140a50a
Caetano Costa <caetanogonca@gmail.com>
2024-10-04 22:59:17 -0300

    acho q foi

---
+++ NYMUS/Assets/TileMaps/Fase Documentos/Fase Documentos.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Fase Documentos" tilewidth="16" tileheight="16" tilecount="10240" columns="128">
+ <image source="main_lev_build - Copia.png" width="2048" height="1280"/>
+</tileset>
