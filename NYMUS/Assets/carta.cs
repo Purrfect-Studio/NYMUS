d1322af43467fc6b11c3b5589724e47f65e8e9b9
Caetano Costa <caetanogonca@gmail.com>
2024-10-08 15:27:50 -0300

    carta cura ✉️❤️

--- NYMUS/Assets/carta.cs
+++ NYMUS/Assets/carta.cs
@@ -7,12 +7,13 @@ public class carta : MonoBehaviour
 {
     public GameObject texto;
     public GameObject tagfeliz;
+    public GameObject jogador;
     // Start is called before the first frame update
     void Start()
     {
         texto = GameObject.FindWithTag("SENHA SECRETA");   
-        tagfeliz = GameObject.FindWithTag("tag19"); 
-      
+        tagfeliz = GameObject.FindWithTag("tag19");
+        jogador = GameObject.FindWithTag("Jogador");
     }
 
     public void metodo()
@@ -20,6 +21,7 @@ public class carta : MonoBehaviour
         texto.GetComponent<TextMeshProUGUI>().text = "8241";
         tagfeliz.GetComponent<ContatoTrigger>().ativarSempre = true;
         tagfeliz.SetActive(true);
+        jogador.GetComponent<VidaJogador>().curar(300);
     }
 
     // Update is called once per frame
